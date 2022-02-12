69bfceea4204d02f96b027f5caedbd513e719e18
ABAcoder <67940201+ABAcoder@users.noreply.github.com>
2022-02-12 18:29:21 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import Phaser from 'phaser';
 import './style.css';
 import Counter from './scenes/counter';
+import {calculator, householdObject, planets} from './calculator';
 
 const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 550,
   transparent: true,
-  scene: [
+  scene: {
     Counter,
-  ],
+  },
 };
 
 const game = new Phaser.Game(config);
+
+calculator(planets.sun, householdObject.pencil);
