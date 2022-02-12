9da68519aa90d97d1c81ba45f74397537e7c216c
Danwo0 <dan.junwoo.lee@gmail.com>
2022-02-12 16:33:43 -0500

    CSS Background
    Added animated background through HTML/CSS

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import './style.css';
 import Counter from './scenes/counter';
 
 const config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 550,
-  transparent: true,
-  scene: {
-    Counter,
-  },
+    type: Phaser.AUTO,
+    width: 800,
+    height: 550,
+    transparent: true,
+    scene: {
+        Counter,
+    },
 };
 
 const game = new Phaser.Game(config);
