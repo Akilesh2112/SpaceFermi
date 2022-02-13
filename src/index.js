1d31548e6254709f54bfb38496f6fc022d3a6c18
Dmitrii Troitskii <mitrafantos@gmail.com>
2022-02-12 22:45:58 -0500

    set up Phaser from sratch and made it work with the stars background

--- src/index.js
+++ src/index.js
@@ -1,29 +1,58 @@
 import Phaser from 'phaser';
-// import './style.css';
-import Counter from './scenes/counter';
+import './style.css';
+
 import { calculator, householdObject, planets } from './calculator';
+
+function preload() {
+  this.load.setBaseURL('http://labs.phaser.io');
+
+  this.load.image('sky', 'assets/skies/space3.png');
+  this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+  this.load.image('red', 'assets/particles/red.png');
+}
+
+function create() {
+  this.add.image(400, 300, 'sky');
+
+  const particles = this.add.particles('red');
+
+  const emitter = particles.createEmitter({
+    speed: 100,
+    scale: { start: 1, end: 0 },
+    blendMode: 'ADD',
+  });
+
+  const logo = this.physics.add.image(400, 100, 'logo');
+
+  logo.setVelocity(100, 200);
+  logo.setBounce(1, 1);
+  logo.setCollideWorldBounds(true);
+
+  emitter.startFollow(logo);
+}
 
 const config = {
   type: Phaser.AUTO,
-  // width: 1200,
-  // height: 500,
   scale: {
-    mode: Phaser.Scale.ScaleModes.FIT,
-    autoCenter: Phaser.Scale.Center.CENTER_BOTH,
+    width: 400,
+    height: 400,
+    // makes the canvas fit the screen
+    // mode: Phaser.Scale.ScaleModes.FIT,
+    // makes the canvas stay in the center
+    // autoCenter: Phaser.Scale.Center.CENTER_BOTH,
   },
-  // zoom: 2,
-  // resolution: window.devicePixelRatio,
-  // we set the canvas for phaser in a specific div
-  // parent: 'phaser',
+  parent: 'phaser',
   transparent: true,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 200 },
+    },
+  },
   scene: {
-    // Counter,
-    create: create,
+    preload,
+    create,
   },
 };
-
-function create() {
-  this.add.text(0, 0, 'Hello World', { font: '"Press Start 2P"' });
-}
 
 const game = new Phaser.Game(config);
